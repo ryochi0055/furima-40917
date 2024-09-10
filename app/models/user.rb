c440c4c4c6bedffdb78d100a8b9db47aff25a47c
ryochi0055 <aredoggu@gmail.com>
2024-09-10 14:09:05 +0900

    ユーザー管理機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,20 @@
 class User < ApplicationRecord
-
-  validates :nickname, presence: true
-  validates :email, presence: true
-  validates :encrypted_password, presence: true
-  validates :birthday, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_rub, presence: true
-  validates :first_name_rub, presence: true
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+  validates :last_name_rub, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: ' is invalid. Input full-width characters' }
+  validates :first_name_rub, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: ' is invalid. Input full-width characters' }
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX, message: ' is invalid. Include both letters and numbers'
+
+  has_many :items
+  has_many :purchase_records
 end
